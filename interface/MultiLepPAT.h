7b8b4431c1d802a03829a6c7b1adddc870ef93ed
Chi Wang <chiw@lxplus933.cern.ch>
2024-07-05 03:04:21 +0200

    :construction: Reviewing, annotating and renamed a few variables.
    :memo: Reviewed MultiLepPAT.cc and added annotation for the first half
    of the code.
    :art: Renamed a few booleans in MultiLepPAT.cc for readability
    :construction: Assigned tag "TO_ENC" and "TO_IMPR_CPP11" to a few places
    for possible improvement.

--- interface/MultiLepPAT.h
+++ interface/MultiLepPAT.h
@@ -1,21 +1,16 @@
 /******************************************************************************
  *  [File] 
- *      MultiLepPAT.cc 
+ *      MultiLepPAT.h 
  *  [Class]      
  *      MultiLepPAT 
  *  [Directory] 
- *      myAnalyzers/MultiLepPAT/src/MultiLepPAT.cc
+ *      TPS-Onia2MuMu/interface/MultiLepPAT.h
  *  [Description]
- *      Make rootTuple for JPsiKKK reconstruction
+ *      Make rootTuple for JPsi+Jpsi+Upsilon reconstruction
  *  [Implementation]
  *     <Notes on implementation>
  *  [Original Author]
  *  
- *  [Update Log]
- *      20240626 [Eric Wang]
- *          Added necessary annotation for the code
- *          Modified for TPS analysis on pp > Jpsi + Jpsi + Ups
- *              (Decay channels: Jpsi > mu- + mu+, Ups > mu- + mu+)
  *  [Note]
  *      20240626 [Eric Wang]
  *          Upsilon is abbreviated as "Ups"
@@ -298,26 +293,22 @@ private:
 
     // Index of selected muons in one event [Added by Eric Wang, 20240626]
     // Reference: code from Wang Xining
-    vector<float> *Jpsi1_mu1_Idx,   *Jpsi1_mu2_Idx, *Jpsi2_mu1_Idx, *Jpsi2_mu2_Idx;
-    vector<float> *Ups1_mu1_Idx,    *Ups1_mu2_Idx;
+    vector<float> *Jpsi_mu1_Idx,    *Jpsi_mu2_Idx;
+    vector<float> *Ups_mu1_Idx,     *Ups_mu2_Idx;
 
     // Vertex fitting [Modified by Eric Wang, 20240626]
     vector<float> *VtxProb,      *Chi2,           *ndof,        *VtxPt2;
 
     // Secondary Jpsi's and Upsilon's kinematics [Modified by Eric Wang, 20240626]
-    vector<float> *Jpsi1_mass,   *Jpsi1_VtxProb,  *Jpsi1_Chi2,  *Jpsi1_ndof,
-                  *Jpsi2_mass,   *Jpsi2_VtxProb,  *Jpsi2_Chi2,  *Jpsi2_ndof,
-                  *Jpsi1_px,     *Jpsi1_py,       *Jpsi1_pz,    *Jpsi1_massErr, 
-                  *Jpsi2_px,     *Jpsi2_py,       *Jpsi2_pz,    *Jpsi2_massErr;
-    vector<float> *Ups_mass,     *Ups_VtxProb,    *Ups_Chi2,    *Ups_ndof,
-                  *Ups_px,       *Ups_py,         *Ups_pz,      *Ups_massErr;
+    vector<float> *Jpsi_mass,   *Jpsi_VtxProb,  *Jpsi_Chi2, *Jpsi_ndof,
+                  *Jpsi_px,     *Jpsi_py,       *Jpsi_pz,   *Jpsi_massErr;
+    vector<float> *Ups_mass,    *Ups_VtxProb,   *Ups_Chi2,  *Ups_ndof,
+                  *Ups_px,      *Ups_py,        *Ups_pz,    *Ups_massErr;
 
-    // Secondary Jpsi's and Upsilon's kinematics fitted under constraints 
+    // Secondary Jpsi's and Upsilon's kinematics fitted under mass constraints 
     //      [Modified by Eric Wang, 20240626]
-    vector<float> *CS_Jpsi1_mass,   *CS_Jpsi1_VtxProb,  *CS_Jpsi1_Chi2,  *CS_Jpsi1_ndof,
-                  *CS_Jpsi2_mass,   *CS_Jpsi2_VtxProb,  *CS_Jpsi2_Chi2,  *CS_Jpsi2_ndof,
-                  *CS_Jpsi1_px,     *CS_Jpsi1_py,       *CS_Jpsi1_pz,    *CS_Jpsi1_massErr, 
-                  *CS_Jpsi2_px,     *CS_Jpsi2_py,       *CS_Jpsi2_pz,    *CS_Jpsi2_massErr;
+    vector<float> *CS_Jpsi_mass,    *CS_Jpsi_VtxProb,   *CS_Jpsi_Chi2,   *CS_Jpsi_ndof,
+                  *CS_Jpsi_px,      *CS_Jpsi_py,        *CS_Jpsi_pz,     *CS_Jpsi_massErr;
     vector<float> *CS_Ups_mass,     *CS_Ups_VtxProb,    *CS_Ups_Chi2,    *CS_Ups_ndof,
                   *CS_Ups_px,       *CS_Ups_py,         *CS_Ups_pz,      *CS_Ups_massErr;
 
@@ -327,160 +318,24 @@ private:
         *MC_G1_Jpsi1_px, *MC_G1_Jpsi1_py, *MC_G1_Jpsi1_pz, *MC_G1_Jpsi1_mass,
         *MC_G1_Jpsi2_px, *MC_G1_Jpsi2_py, *MC_G1_Jpsi2_pz, *MC_G1_Jpsi2_mass,
         *MC_G1_Ups1_px,  *MC_G1_Ups1_py,  *MC_G1_Ups1_pz,  *MC_G1_Ups1_mass;
+
+    // For Muon pairs, always keep the order of mu1 = mu+ and mu2 = mu-; 
     vector<float> 
         *MC_G2_Mu1_px, *MC_G2_Mu1_py, *MC_G2_Mu1_pz, *MC_G2_Mu1_mass,
-        *MC_G2_Mu2_px, *MC_G2_Mu2_py, *MC_G2_Mu2_pz, *MC_G2_Mu2_mass,
-        *MC_G2_Mu3_px, *MC_G2_Mu3_py, *MC_G2_Mu3_pz, *MC_G2_Mu3_mass,
-        *MC_G2_Mu4_px, *MC_G2_Mu4_py, *MC_G2_Mu4_pz, *MC_G2_Mu4_mass,
-        *MC_G2_Mu5_px, *MC_G2_Mu5_py, *MC_G2_Mu5_pz, *MC_G2_Mu5_mass,
-        *MC_G2_Mu6_px, *MC_G2_Mu6_py, *MC_G2_Mu6_pz, *MC_G2_Mu6_mass ;
+        *MC_G2_Mu2_px, *MC_G2_Mu2_py, *MC_G2_Mu2_pz, *MC_G2_Mu2_mass ; 
 
     // Particle ID
 
     vector<float> 
-        *MC_G1_Jpsi1_PDG_ID,
-        *MC_G1_Jpsi2_PDG_ID,
-        *MC_G1_Ups1_PDG_ID;
+        *MC_G1_Jpsi_PDG_ID,
+        *MC_G1_Ups_PDG_ID;
     vector<float> 
         *MC_G2_Mu1_PDG_ID, 
-        *MC_G2_Mu2_PDG_ID, 
-        *MC_G2_Mu3_PDG_ID, 
-        *MC_G2_Mu4_PDG_ID, 
-        *MC_G2_Mu5_PDG_ID, 
-        *MC_G2_Mu6_PDG_ID;
+        *MC_G2_Mu2_PDG_ID ; 
     vector<float> 
         *Match_mu1px, *Match_mu1py, *Match_mu1pz,
-        *Match_mu2px, *Match_mu2py, *Match_mu2pz,
-        *Match_mu3px, *Match_mu3py, *Match_mu3pz,
-        *Match_mu4px, *Match_mu4py, *Match_mu4pz,
-        *Match_mu5px, *Match_mu5py, *Match_mu5pz,
-        *Match_mu6px, *Match_mu6py, *Match_mu6pz ;
+        *Match_mu2px, *Match_mu2py, *Match_mu2pz ; 
 
-    
-
-// below are removed from the main code [Modified by Eric Wang, 20240626]
-/*
-    //xining
-    // 
-    vector<float> 
-        *X_mu1Idx,       *X_mu2Idx,         *X_mu3Idx,      *X_mu4Idx,
-        *X_mass,         *X_VtxProb,        *X_Chi2,        *X_ndof, 
-        *X_px,           *X_py,             *X_pz,          *X_massErr, 
-        *X_JPiPi_mass,   *X_JPiPi_VtxProb,  *X_JPiPi_Chi2,  *X_JPiPi_ndof,
-        *X_Jpsi1_mass,   *X_Jpsi1_VtxProb,  *X_Jpsi1_Chi2,  *X_Jpsi1_ndof,
-        *X_Jpsi2_mass,   *X_Jpsi2_VtxProb,  *X_Jpsi2_Chi2,  *X_Jpsi2_ndof,
-        *X_JPiPi_px,     *X_JPiPi_py,       *X_JPiPi_pz,    *X_JPiPi_massErr, 
-        *X_Jpsi1_px,     *X_Jpsi1_py,       *X_Jpsi1_pz,    *X_Jpsi1_massErr, 
-        *X_Jpsi2_px,     *X_Jpsi2_py,       *X_Jpsi2_pz,    *X_Jpsi2_massErr, 
-        *X_JPiPi_Pi1Idx, *X_JPiPi_Pi2Idx,
-        *X_JPiPi_Pi1px,  *X_JPiPi_Pi1py,    *X_JPiPi_Pi1pz,
-        *X_JPiPi_Pi2px,  *X_JPiPi_Pi2py,    *X_JPiPi_Pi2pz; 
-
-    //added for mass constrain on 1208, cs stands for some kind of mass constraint
-    // Variables for Jpsi and Pion
-    vector <float>  *cs_X_Jpsi1_mass, *cs_X_Jpsi1_VtxProb,  *cs_X_Jpsi1_Chi2,   *cs_X_Jpsi1_ndof, 
-                    *cs_X_Jpsi2_mass, *cs_X_Jpsi2_VtxProb,  *cs_X_Jpsi2_Chi2,   *cs_X_Jpsi2_ndof, 
-                    *cs_X_Jpsi1_px,   *cs_X_Jpsi1_py,       *cs_X_Jpsi1_pz,     *cs_X_Jpsi1_massErr,
-                    *cs_X_Jpsi2_px,   *cs_X_Jpsi2_py,       *cs_X_Jpsi2_pz,     *cs_X_Jpsi2_massErr        ;
-    // Notice that Jpsi and Pion information is actually the same as before, 
-    // because JPiPi MC fit doesn't change them by default.  
-    // So Jpsi information retrived from Jpsi MC fit but not JPiPi MC fit. 
-    // And Pion information is actually the same as that in 4mu2pi X fit so there are no variables for them
-    // If you do what to change them, please check JPiPi MC fit and information stored carefully.
-
-    // Variables for JPiPi fitted with MassConstraint Jpsi1
-    vector<float> *cs_X_JPiPi_mass, *cs_X_JPiPi_VtxProb,    *cs_X_JPiPi_Chi2,   *cs_X_JPiPi_ndof, 
-                  *cs_X_JPiPi_px ,  *cs_X_JPiPi_py,         *cs_X_JPiPi_pz,     *cs_X_JPiPi_massErr ;
-
-    // Variables for X and JPiPi; MassConstraint to Psi2S and X3782 
-    vector<float> *cs_X_mass_Psi2S,         *cs_X_VtxProb_Psi2S,        *cs_X_Chi2_Psi2S,       *cs_X_ndof_Psi2S, 
-                  *cs_X_px_Psi2S,           *cs_X_py_Psi2S,             *cs_X_pz_Psi2S,         *cs_X_massErr_Psi2S,
-                  *cs_X_JPiPi_mass_Psi2S,   *cs_X_JPiPi_VtxProb_Psi2S,  *cs_X_JPiPi_Chi2_Psi2S, *cs_X_JPiPi_ndof_Psi2S, 
-                  *cs_X_JPiPi_px_Psi2S,     *cs_X_JPiPi_py_Psi2S,       *cs_X_JPiPi_pz_Psi2S,   *cs_X_JPiPi_massErr_Psi2S;
-
-    vector<float> *cs_X_mass_X3872,         *cs_X_VtxProb_X3872,        *cs_X_Chi2_X3872,       *cs_X_ndof_X3872, 
-                  *cs_X_px_X3872,           *cs_X_py_X3872,             *cs_X_pz_X3872,         *cs_X_massErr_X3872,
-                  *cs_X_JPiPi_mass_X3872,   *cs_X_JPiPi_VtxProb_X3872,  *cs_X_JPiPi_Chi2_X3872, *cs_X_JPiPi_ndof_X3872, 
-                  *cs_X_JPiPi_px_X3872,     *cs_X_JPiPi_py_X3872,       *cs_X_JPiPi_pz_X3872,   *cs_X_JPiPi_massErr_X3872;
-
-
-//doMC
-vector<float> 
-*MC_X_px,
-*MC_X_py,
-*MC_X_pz,
-*MC_X_mass,
-*MC_Dau_Jpsipx,
-*MC_Dau_Jpsipy,
-*MC_Dau_Jpsipz,
-*MC_Dau_Jpsimass,
-*MC_Dau_psi2spx,
-*MC_Dau_psi2spy,
-*MC_Dau_psi2spz,
-*MC_Dau_psi2smass,
-*MC_Granddau_mu1px,
-*MC_Granddau_mu1py,
-*MC_Granddau_mu1pz,
-*MC_Granddau_mu2px,
-*MC_Granddau_mu2py,
-*MC_Granddau_mu2pz,
-*MC_Granddau_Jpsipx,
-*MC_Granddau_Jpsipy,
-*MC_Granddau_Jpsipz,
-*MC_Granddau_Jpsimass,
-*MC_Granddau_pi1px,
-*MC_Granddau_pi1py,
-*MC_Granddau_pi1pz,
-*MC_Granddau_pi2px,
-*MC_Granddau_pi2py,
-*MC_Granddau_pi2pz,
-*MC_Grandgranddau_mu3px,
-*MC_Grandgranddau_mu3py,
-*MC_Grandgranddau_mu3pz,
-*MC_Grandgranddau_mu4px,
-*MC_Grandgranddau_mu4py,
-*MC_Grandgranddau_mu4pz;
-
-vector<int>
-*MC_X_chg,
-*MC_Dau_JpsipdgId,
-*MC_Dau_psi2spdgId,
-*MC_Granddau_mu1pdgId,
-*MC_Granddau_mu2pdgId,
-*MC_Granddau_JpsipdgId,
-*MC_Granddau_pi1pdgId,
-*MC_Granddau_pi2pdgId,
-*MC_Grandgranddau_mu3pdgId,
-*MC_Grandgranddau_mu4pdgId;
-
-vector<float> 
-*Match_mu1px,
-*Match_mu1py,
-*Match_mu1pz,
-*Match_mu2px,
-*Match_mu2py,
-*Match_mu2pz,
-*Match_mu3px,
-*Match_mu3py,
-*Match_mu3pz,
-*Match_mu4px,
-*Match_mu4py,
-*Match_mu4pz,
-
-*Match_pi1px,
-*Match_pi1py,
-*Match_pi1pz,
-*Match_pi2px,
-*Match_pi2py,
-*Match_pi2pz; 
-
-  // float mybxlumicorr,myrawbxlumi;
-  ////////////////////////  
-
-  
-
-
-*/
   
 };
 
